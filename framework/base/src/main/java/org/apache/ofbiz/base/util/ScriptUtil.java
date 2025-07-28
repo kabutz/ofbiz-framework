057b423a0054da101eb83f52c52a4e25238aaa34
Dr Heinz M. Kabutz <heinz@javaspecialists.eu>
2025-07-28 18:12:38 +0300

    task 4 done - textblocks

--- framework/base/src/main/java/org/apache/ofbiz/base/util/ScriptUtil.java
+++ framework/base/src/main/java/org/apache/ofbiz/base/util/ScriptUtil.java
@@ -422,9 +422,13 @@ public final class ScriptUtil {
         boolean currentScriptAlreadyAllowed = allowedScript.contains(scriptHash);
         if (!currentScriptAlreadyAllowed) {
             if (!checkIfScriptIsSafe(script)) {
-                // REFACTOR: Replace fragmented Strings with Text Blocks
-                Debug.logWarning(String.format("Tried to execute unauthorized script \n **** \n%s\n **** "
-                                + "\nif it's safe script you can add the following hash to security.allowedScriptletHashes: %s",
+                // REFACTO: Replace fragmented Strings with Text Blocks
+                Debug.logWarning(String.format("""
+                                Tried to execute unauthorized script
+                                 ****
+                                %s
+                                 ****
+                                if it's safe script you can add the following hash to security. allowedScriptletHashes: %s""",
                         script, scriptHash), MODULE);
                 return false;
             }
